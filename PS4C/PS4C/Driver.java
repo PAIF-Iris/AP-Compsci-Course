b72e85b67798be27452985df08405202853927b3
PAIF-Iris <iris.guo2007@outlook.com>
2024-11-04 13:31:01 -0800

    Add files via upload

---
+++ PS4C/PS4C/Driver.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class Driver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Driver
+{
+    public static void main (String args[]) {
+        double num = Review.sentimentVal("beautiful");
+        System.out.println(num);
+        
+        System.out.println(Review.totalSentiment("SimpleReview.txt"));
+        System.out.println(Review.starRating("SimpleReview.txt"));
+
+        System.out.println(Review.totalSentiment("YelpReview.txt"));
+        System.out.println(Review.starRating("YelpReview.txt"));
+
+
+    }
+}
